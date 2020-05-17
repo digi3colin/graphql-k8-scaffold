7120293eeb384fa461137ddb0bcc6ad62cf5f5bc
Colin Leung <colin@cowise.co>
2020-05-18 00:07:09 +0800

    draft code gen.

---
+++ index.js
@@ -0,0 +1,167 @@
+const {
+  getFieldType,
+  getTypeMap
+} = require('@komino/graphql-sqlite-ddl');
+
+const pluralize = require('pluralize');
+pluralize.addPluralRule('person', 'persons');
+const {snakeCase} = require('snake-case');
+
+function getDefaultValue(value, type){
+  switch(type){
+    case 'Boolean':
+      return value ? "true" : "false";
+    case 'String':
+      return `"${value}"`;
+    case 'Int':
+    case 'Float':
+    default:
+      return value;
+  }
+}
+
+
+const typeToFields = (type)  =>{
+  const defaultValues = new Map();
+
+  type.fields.forEach(field => {
+    const name = field.name.value;
+
+    const isORMFields = /^(id|created_at|updated_at)$/.test(name);
+    const isBelongs = /^belongsTo/.test(name);
+    const isAssoicateTo = /^associateTo/.test(name);
+    const isHasAndBelongsToMany = /^hasAndBelongsToMany/.test(name);
+    if ( isORMFields || isBelongs || isAssoicateTo || isHasAndBelongsToMany) return;
+
+    const fieldType = getFieldType(field.type);
+
+    field.directives.map( directive => {
+      switch( directive.name.value ){
+        case "default":
+          defaultValues.set(name, getDefaultValue(directive.arguments[0].value.value, fieldType))
+          break;
+      }
+    });
+
+    defaultValues.set(name, defaultValues.get(name) || "null")
+  });
+
+
+  return defaultValues;
+}
+
+const typeToForeignKeys = (type) =>{
+  const result = new Map();
+
+  type.fields.forEach(field => {
+    const name = field.name.value;
+
+    const isBelongs = /^belongsTo/.test(name);
+    const isAssoicateTo = /^associateTo/.test(name);
+    if( isBelongs || isAssoicateTo){
+      const model = snakeCase(field.type.name.value);
+      let fk = pluralize.singular(model) + '_id'
+
+      //check custom foreign key rather than model_id
+      field.directives.forEach( directive => {
+        switch (directive.name.value) {
+          case "foreignKey":
+            fk = directive.arguments[0].value.value;
+            break;
+        }
+      });
+      result.set(fk, pluralize.singular(field.type.name.value))
+    }
+  });
+
+  return result;
+}
+
+const typeToBelongsToMany = (type) =>{
+  const result = [];
+
+  type.fields.forEach(field => {
+    const name = field.name.value;
+
+    const isHasAndBelongsToMany = /^hasAndBelongsToMany/.test(name);
+    if( isHasAndBelongsToMany ){
+      result.push(pluralize.singular(field.type.name.value))
+    }
+  });
+
+  return result;
+}
+
+const parseType = (type) =>{
+  const tableName = snakeCase(pluralize(type.name.value));
+  const className = pluralize.singular(type.name.value);
+  const defaultValues = typeToFields(type);
+
+  const belongsTo = type.belongsTo;
+  const hasMany = type.hasMany || new Map();
+  const belongsToMany = typeToBelongsToMany(type);
+
+  return `const {K8} = require('@komino/k8');
+const ORM = K8.require('ORM');
+
+class ${className} extends ORM{
+  constructor(id, options) {
+    super(id, options);
+    if(id)return;
+
+    //foreignKeys
+${Array.from(belongsTo.keys()).map(x => `    this.${x} = null;`).join('\n')}
+
+    //fields
+${Array.from(defaultValues).map((x ) => `    this.${x[0]} = ${x[1]};`).join('\n')}
+  }
+}
+
+${className}.jointTablePrefix = '${snakeCase(className)}';
+${className}.tableName = '${tableName}';
+
+${className}.fields = [
+${Array.from(defaultValues).map(x => `"${x[0]}"`).join(', ')}
+];
+
+${className}.belongsTo = new Map([
+${Array.from(belongsTo).map(x => `["${x[0]}", "${x[1]}"]`).join(',\n')}
+]);
+
+${className}.hasMany = new Map([
+${Array.from(hasMany).map(x => `["${x[0]}", "${x[1]}"]`).join(',\n')}
+]);
+
+${className}.belongsToMany = [
+${Array.from(belongsToMany).map(x => `"${x}"`).join(',\n')}
+];
+
+module.exports = ${className};
+`;
+}
+
+const codeGen = (schema) =>{
+  const codes = [];
+  const typeMap = getTypeMap(schema);
+
+//revserse belongs to and associate to create hasMany
+  typeMap.forEach((type, key) =>{
+    type.belongsTo = typeToForeignKeys(type);
+
+    type.belongsTo.forEach((v, k) => {
+      const t = typeMap.get(pluralize(v))
+      t.hasMany = t.hasMany || new Map();
+      t.hasMany.set(k, pluralize.singular(key));
+    })
+  });
+
+  typeMap.forEach(type =>{
+    codes.push(parseType(type));
+  })
+
+  return codes.join('');
+}
+
+module.exports = {
+  codeGen : codeGen,
+}
---
+++ tests/genModel.test.js
@@ -0,0 +1,304 @@
+const { buildSchema } = require('graphql');
+const {schemaHeader} = require('@komino/graphql-sqlite-ddl');
+const {codeGen} = require('../index');
+
+describe('test schema to model', () => {
+  test('simple schema', () => {
+    const schema = buildSchema(`
+type Persons {
+    first_name: String!
+    last_name: String!
+    phone: String
+    email: String
+}`);
+
+    const model = codeGen(schema);
+    const target =
+`const {K8} = require('@komino/k8');
+const ORM = K8.require('ORM');
+
+class Person extends ORM{
+  constructor(id, options) {
+    super(id, options);
+    if(id)return;
+
+    //foreignKeys
+
+
+    //fields
+    this.first_name = null;
+    this.last_name = null;
+    this.phone = null;
+    this.email = null;
+  }
+}
+
+Person.jointTablePrefix = 'person';
+Person.tableName = 'persons';
+
+Person.fields = [
+"first_name", "last_name", "phone", "email"
+];
+
+Person.belongsTo = new Map([
+
+]);
+
+Person.hasMany = new Map([
+
+]);
+
+Person.belongsToMany = [
+
+];
+
+module.exports = Person;
+`
+
+    expect(model).toBe(target);
+
+  })
+
+  test('default value', () => {
+    const schema = buildSchema(schemaHeader + `
+type Persons {
+    name: String!
+    foo: Boolean! @default(value: true)
+    koo: Boolean! @default(value: false)
+    bar: String! @default(value: "yoo")
+    tar: Float! @default(value: 0.5)
+    haa: Int! @default(value: 100)
+}`);
+
+    const model = codeGen(schema);
+    const target =
+      `const {K8} = require('@komino/k8');
+const ORM = K8.require('ORM');
+
+class Person extends ORM{
+  constructor(id, options) {
+    super(id, options);
+    if(id)return;
+
+    //foreignKeys
+
+
+    //fields
+    this.name = null;
+    this.foo = true;
+    this.koo = false;
+    this.bar = "yoo";
+    this.tar = 0.5;
+    this.haa = 100;
+  }
+}
+
+Person.jointTablePrefix = 'person';
+Person.tableName = 'persons';
+
+Person.fields = [
+"name", "foo", "koo", "bar", "tar", "haa"
+];
+
+Person.belongsTo = new Map([
+
+]);
+
+Person.hasMany = new Map([
+
+]);
+
+Person.belongsToMany = [
+
+];
+
+module.exports = Person;
+`
+
+    expect(model).toBe(target);
+
+  })
+
+  test('foreign Keys', ()=>{
+    const schema = buildSchema(schemaHeader + `
+type Users{
+    name: String
+}
+
+type Blogs{
+    handle: String 
+    belongsTo: Users @foreignKey(value: "owner_id")
+    
+}
+ `);
+
+    const model = codeGen(schema);
+    const target =
+`const {K8} = require('@komino/k8');
+const ORM = K8.require('ORM');
+
+class User extends ORM{
+  constructor(id, options) {
+    super(id, options);
+    if(id)return;
+
+    //foreignKeys
+
+
+    //fields
+    this.name = null;
+  }
+}
+
+User.jointTablePrefix = 'user';
+User.tableName = 'users';
+
+User.fields = [
+"name"
+];
+
+User.belongsTo = new Map([
+
+]);
+
+User.hasMany = new Map([
+["owner_id", "Blog"]
+]);
+
+User.belongsToMany = [
+
+];
+
+module.exports = User;
+const {K8} = require('@komino/k8');
+const ORM = K8.require('ORM');
+
+class Blog extends ORM{
+  constructor(id, options) {
+    super(id, options);
+    if(id)return;
+
+    //foreignKeys
+    this.owner_id = null;
+
+    //fields
+    this.handle = null;
+  }
+}
+
+Blog.jointTablePrefix = 'blog';
+Blog.tableName = 'blogs';
+
+Blog.fields = [
+"handle"
+];
+
+Blog.belongsTo = new Map([
+["owner_id", "User"]
+]);
+
+Blog.hasMany = new Map([
+
+]);
+
+Blog.belongsToMany = [
+
+];
+
+module.exports = Blog;
+`
+
+    expect(model).toBe(target);
+  })
+
+  test('hasAndBelongsToMany', ()=>{
+    const schema = buildSchema(schemaHeader + `
+type Product{
+    name: String
+}
+
+type Collections{
+    name: String 
+    hasAndBelongsToMany: Product
+}
+ `);
+
+    const target =
+`const {K8} = require('@komino/k8');
+const ORM = K8.require('ORM');
+
+class Product extends ORM{
+  constructor(id, options) {
+    super(id, options);
+    if(id)return;
+
+    //foreignKeys
+
+
+    //fields
+    this.name = null;
+  }
+}
+
+Product.jointTablePrefix = 'product';
+Product.tableName = 'products';
+
+Product.fields = [
+"name"
+];
+
+Product.belongsTo = new Map([
+
+]);
+
+Product.hasMany = new Map([
+
+]);
+
+Product.belongsToMany = [
+
+];
+
+module.exports = Product;
+const {K8} = require('@komino/k8');
+const ORM = K8.require('ORM');
+
+class Collection extends ORM{
+  constructor(id, options) {
+    super(id, options);
+    if(id)return;
+
+    //foreignKeys
+
+
+    //fields
+    this.name = null;
+  }
+}
+
+Collection.jointTablePrefix = 'collection';
+Collection.tableName = 'collections';
+
+Collection.fields = [
+"name"
+];
+
+Collection.belongsTo = new Map([
+
+]);
+
+Collection.hasMany = new Map([
+
+]);
+
+Collection.belongsToMany = [
+"Product"
+];
+
+module.exports = Collection;
+`;
+
+    const model = codeGen(schema);
+    expect(model).toBe(target);
+  })
+});
